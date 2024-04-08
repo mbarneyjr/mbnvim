99bf9694f0d94b3af0cba18dbe16baa2171733fd
Michael Barney, Jr <mbarneyme@gmail.com>
2024-04-08 13:54:07 -0400

    feat(snippets): add custom snippets for node tests

--- lua/barney/plugins/nvim-cmp.lua
+++ lua/barney/plugins/nvim-cmp.lua
@@ -13,7 +13,21 @@ return {
     local luasnip = require("luasnip")
 
     require("luasnip.loaders.from_vscode").lazy_load()
+    vim.keymap.set({ "i" }, "<C-K>", function()
+      luasnip.expand()
+    end, { silent = true })
+    vim.keymap.set({ "i", "s" }, "<C-L>", function()
+      luasnip.jump(1)
+    end, { silent = true })
+    vim.keymap.set({ "i", "s" }, "<C-J>", function()
+      luasnip.jump(-1)
+    end, { silent = true })
 
+    vim.keymap.set({ "i", "s" }, "<C-E>", function()
+      if luasnip.choice_active() then
+        luasnip.change_choice(1)
+      end
+    end, { silent = true })
     cmp.setup({
       completion = { completeopt = "menu,menuone,preview,noselect" },
       snippet = {
@@ -50,6 +64,54 @@ return {
       }),
       luasnip.snippet("transform", {
         luasnip.text_node("Transform: AWS::Serverless-2016-10-31"),
+      }),
+      -- create a node:test test file snippet
+      luasnip.snippet("nodetestfile", {
+        luasnip.text_node({
+          "import { describe, it, beforeEach, afterEach, mock } from 'node:test';",
+          "import assert from 'node:assert/strict';",
+          "",
+          "await describe('",
+        }),
+        luasnip.insert_node(1),
+        luasnip.text_node({
+          "', async () => {",
+          "  beforeEach(() => {",
+          "    mock.reset();",
+          "  });",
+          "  afterEach(() => {",
+          "    mock.restoreAll();",
+          "  });",
+          "",
+          "  await it('",
+        }),
+        luasnip.insert_node(2),
+        luasnip.text_node({
+          "', async () => {",
+          "    ",
+        }),
+        luasnip.insert_node(3),
+        luasnip.text_node({
+          "",
+          "  });",
+          "});",
+        }),
+      }),
+      -- create a node:test individual test snippet
+      luasnip.snippet("nodetest", {
+        luasnip.text_node({
+          "  await it('",
+        }),
+        luasnip.insert_node(1),
+        luasnip.text_node({
+          "', async () => {",
+          "    ",
+        }),
+        luasnip.insert_node(2),
+        luasnip.text_node({
+          "",
+          "  });",
+        }),
       }),
     })
   end,
