092ef7eb379e6442c7a8754d06c4fc4b62f93df2
Michael Barney, Jr <mbarneyme@gmail.com>
2024-04-08 14:43:37 -0400

    fix(snippets): resolve keymap conflicts with snippets

--- lua/barney/plugins/nvim-cmp.lua
+++ lua/barney/plugins/nvim-cmp.lua
@@ -13,20 +13,8 @@ return {
     local luasnip = require("luasnip")
 
     require("luasnip.loaders.from_vscode").lazy_load()
-    vim.keymap.set({ "i" }, "<C-K>", function()
-      luasnip.expand()
-    end, { silent = true })
-    vim.keymap.set({ "i", "s" }, "<C-L>", function()
-      luasnip.jump(1)
-    end, { silent = true })
     vim.keymap.set({ "i", "s" }, "<C-J>", function()
-      luasnip.jump(-1)
-    end, { silent = true })
-
-    vim.keymap.set({ "i", "s" }, "<C-E>", function()
-      if luasnip.choice_active() then
-        luasnip.change_choice(1)
-      end
+      luasnip.jump(1)
     end, { silent = true })
     cmp.setup({
       completion = { completeopt = "menu,menuone,preview,noselect" },
