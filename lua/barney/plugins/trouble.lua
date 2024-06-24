29ee6ce7592bb9f394a003c31d00c01268deff45
Michael Barney, Jr <mbarneyme@gmail.com>
2024-06-24 16:28:48 -0400

    fix(trouble): use default configuration

--- lua/barney/plugins/trouble.lua
+++ lua/barney/plugins/trouble.lua
@@ -2,19 +2,7 @@ return {
   "folke/trouble.nvim",
   config = function()
     local key = require("barney.lib.keymap")
-    require("trouble").setup({
-      icons = false,
-      fold_open = "v",
-      fold_closed = ">",
-      indent_lines = true,
-      signs = {
-        error = "[ERROR]",
-        warning = "[WARN]",
-        hint = "[HINT]",
-        information = "[INFO]",
-      },
-      use_diagnostic_signs = false,
-    })
+    require("trouble").setup()
     key.nmap("<leader>dl", "<cmd>Trouble<cr>", "[d]iagnostics [l]ist")
   end,
 }
