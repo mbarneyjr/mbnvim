c1c188ac8d19468759e59c0e6802361c8e9b0e1b
Michael Barney, Jr <mbarneyme@gmail.com>
2024-07-31 11:19:26 -0400

    feat(linting): re-enable eslint for js and ts files

--- lua/barney/plugins/linting.lua
+++ lua/barney/plugins/linting.lua
@@ -9,8 +9,8 @@ return {
     -- define github actions file type
     vim.filetype.add({})
     lint.linters_by_ft = {
-      -- javascript = { "eslint_d" },
-      -- typescript = { "eslint_d" },
+      javascript = { "eslint_d" },
+      typescript = { "eslint_d" },
       -- ["yaml.cloudformation"] = { "cfn_lint" },
       ["yaml.github_actions"] = { "actionlint" },
     }
