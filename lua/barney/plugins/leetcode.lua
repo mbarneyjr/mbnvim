7623f516a75e1ac41eba4c6d96852a493df0e2e2
Michael Barney, Jr <mbarneyme@gmail.com>
2024-09-20 19:48:10 -0400

    feat(leetcode): add leetcode plugin

---
+++ lua/barney/plugins/leetcode.lua
@@ -0,0 +1,11 @@
+return {
+  "kawre/leetcode.nvim",
+  build = ":TSUpdate html",
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+  },
+  opts = {
+    lang = "javascript",
+    -- configuration goes here
+  },
+}
