86eff23d8b01e6f3808dcbc68454c55f392f6d03
Michael Barney, Jr <mbarneyme@gmail.com>
2025-09-02 10:13:46 -0400

    feat(cedar): update cedar-language-server to latest

--- nix/overlays/cedar-language-server.nix
+++ nix/overlays/cedar-language-server.nix
@@ -5,10 +5,10 @@ final: prev: {
     src = prev.pkgs.fetchFromGitHub {
       owner = "cedar-policy";
       repo = "cedar";
-      rev = "1475ea3b1f79e2f14fb05e95aab0d51f0ea1af5a";
-      sha256 = "sha256-ETYr7cKdrGenoPbZ/09vdY0sJzFPjk32KO/dwUOSlhc=";
+      rev = "8c7fb87043bc65600f6f09c291dbde37ee670071";
+      sha256 = "sha256-/amq3jU+dLgLXnnjCqqCL/WW1kerVtyA/+5yVa1BzIc=";
     };
-    cargoHash = "sha256-qDbVmmtDVd10jNmFTvVnji0yaMeEerVpMipe2gGsWP8=";
+    cargoHash = "sha256-hO8fHvJOGk1FOzm3o6bN+D+1Gdh6Yri6X9ZmGhZ1wTs=";
     buildAndTestSubdir = "cedar-language-server";
   };
 }
