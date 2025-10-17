c5fe9410271b2afbcd52b5c20a8ed42627c926bb
Michael Barney, Jr <mbarneyme@gmail.com>
2025-10-17 10:48:49 -0400

    feat(lua_ls): add neovim runtime to workspace library

--- lua/barney/plugins/lsp/lua.lua
+++ lua/barney/plugins/lsp/lua.lua
@@ -1,1 +1,17 @@
+vim.lsp.config("lua_ls", {
+  settings = {
+    Lua = {
+      workspace = {
+        library = vim.api.nvim_get_runtime_file("", true),
+        checkThirdParty = false,
+      },
+      diagnostics = {
+        globals = { "vim" },
+      },
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+})
 vim.lsp.enable("lua_ls")
