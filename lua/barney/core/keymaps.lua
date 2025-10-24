a581a288aa3d2a6b269b474e5c87b6d9bdf1f6b9
Michael Barney, Jr <mbarneyme@gmail.com>
2025-10-24 08:52:49 -0400

    fix(cleanup): clean options and keymaps

--- lua/barney/core/keymaps.lua
+++ lua/barney/core/keymaps.lua
@@ -8,11 +8,20 @@ key.nmap("<leader>sh", "<C-w>s", "Split window horizontally")
 key.nmap("<leader>se", "<C-w>=", "Make splits equal size")
 key.nmap("<leader>st", ":tab split<CR>", "Copy window to new tab")
 key.nmap("<leader>sx", "<cmd>close<CR>", "Close current split")
+key.nmap("<leader>bp", "<cmd>bprevious<CR>", "Previous buffer")
+key.nmap("<leader>bn", "<cmd>bnext<CR>", "Next buffer")
 
 key.nmap("<c-_>", "<cmd>noh<cr>", "Clear search highlight")
 
--- keymap leader w to toggle wrap
 key.nmap("<leader>w", "<cmd>set wrap!<CR>", "Toggle wrap")
+
+key.nmap("<C-M-j>", "<cmd>m .+1<CR>==", "Move line down")
+key.nmap("<C-M-k>", "<cmd>m .-2<CR>==", "Move line up")
+key.vmap("<C-M-j>", ":m '>+1<CR>gv=gv", "Move selection down")
+key.vmap("<C-M-k>", ":m '<-2<CR>gv=gv", "Move selection up")
+
+key.vmap("<", "<gv", "Indent left and reselect")
+key.vmap(">", ">gv", "Indent right and reselect")
 
 -- quickfixlist keymaps
 key.nmap("<leader>qo", "<cmd>copen<CR>", "Open quickfix list")
--- lua/barney/core/options.lua
+++ lua/barney/core/options.lua
@@ -1,39 +1,28 @@
-vim.opt.nu = true
+vim.opt.number = true
 vim.opt.relativenumber = true
-
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
-
 vim.opt.wrap = false
-
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
-
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undodir = vim.fn.stdpath("data") .. "/undo"
 vim.opt.undofile = true
-
 vim.opt.hlsearch = true
 vim.opt.incsearch = true
-
 vim.opt.scrolloff = 8
+vim.opt.sidescrolloff = 8
 vim.opt.updatetime = 50
 vim.opt.colorcolumn = "120"
-
+vim.opt.termguicolors = true
 vim.opt.completeopt = "menuone,noselect"
-
 vim.opt.clipboard = "unnamed,unnamedplus"
 vim.opt.splitright = true
-
-vim.g.do_filetype_lua = true
-vim.g.did_load_filetypes = false
-
-vim.o.exrc = true
-
--- set tabs to be visible with white vertical line to the left
+vim.opt.autoread = true
+vim.opt.winborder = "rounded"
 vim.opt.list = true
 vim.opt.listchars = {
   tab = "⎸ ",
