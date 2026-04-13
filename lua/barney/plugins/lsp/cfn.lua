c66eb614107e7983f3adb7a336ae8b96cba242b0
Michael Barney, Jr <mbarneyme@gmail.com>
2026-04-13 17:06:18 -0400

    fix(lsp): disable guard rules with cloudformation language server

--- lua/barney/plugins/lsp/cfn.lua
+++ lua/barney/plugins/lsp/cfn.lua
@@ -2,6 +2,18 @@ vim.lsp.config("cfn-lsp-server", {
   cmd = { "cfn-lsp-server", "--stdio" },
   filetypes = { "yaml.cloudformation", "json.cloudformation" },
   root_markers = { ".git" },
-  settings = { validate = false },
+  settings = {
+    editor = {
+      detectIndentation = true,
+    },
+    ["aws.cloudformation"] = {
+      diagnostics = {
+        cfnGuard = {
+          enabled = true,
+          enabledRulePacks = {},
+        },
+      },
+    },
+  },
 })
 vim.lsp.enable("cfn-lsp-server")
