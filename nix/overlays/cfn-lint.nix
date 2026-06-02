8b71d49835d49196ebba46949733ca330ad5548f
Michael Barney, Jr <mbarneyme@gmail.com>
2026-06-02 10:20:44 -0400

    feat(cfn-lint): update to 1.51.2

--- nix/overlays/cfn-lint.nix
+++ nix/overlays/cfn-lint.nix
@@ -2,18 +2,19 @@ final: prev: {
   python3Packages = prev.python3Packages // {
     cfn-lint = (
       prev.python3Packages.cfn-lint.overridePythonAttrs (old: rec {
-        version = "1.44.0";
+        version = "1.51.2";
         src = prev.fetchFromGitHub {
           owner = "aws-cloudformation";
           repo = "cfn-lint";
           tag = "v${version}";
-          hash = "sha256-5Y2MxaZHH2c+VhwfUUzSSGnC1G1FRB7uin/r6JAJBjA=";
+          hash = "sha256-IfBm/4kfYa+Let62hXTlwyrAM8sp+FN1vV6LOTLdQX0=";
         };
         nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [
           prev.python3Packages.setuptools
         ];
         patchPhase = ''
           substituteInPlace pyproject.toml --replace-fail '"setuptools >= 80.10.2"' '"setuptools"'
+          substituteInPlace requirements/base.txt --replace-fail 'aws-sam-translator>=1.110.0' 'aws-sam-translator'
         '';
         doCheck = false;
       })
