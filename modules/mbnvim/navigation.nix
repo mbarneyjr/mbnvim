bc951f925ffb7dd2a53623445b28d0e953479d92
Michael Barney, Jr <mbarneyme@gmail.com>
2026-08-16 16:41:32 -0400

    feat(nvim-tree): pin nvim-tree.lua to unstable GitHub revision

--- modules/mbnvim/navigation.nix
+++ modules/mbnvim/navigation.nix
@@ -1,6 +1,19 @@
 {
   perSystem =
     { inputs', pkgs, ... }:
+    let
+      nvim-tree-lua = pkgs.vimUtils.buildVimPlugin {
+        pname = "nvim-tree.lua";
+        version = "1.18.0-unstable-2026-08-04";
+        src = pkgs.fetchFromGitHub {
+          owner = "nvim-tree";
+          repo = "nvim-tree.lua";
+          rev = "b2aadda94b107480c48e548d6db51c6840b7b33c";
+          hash = "sha256-ILF6IGHYy32tfOnhD4mUVneQXQz2PlaARahJQzJAwsQ=";
+        };
+        meta.homepage = "https://github.com/nvim-tree/nvim-tree.lua/";
+      };
+    in
     {
       mbnvim.plugins = with pkgs.vimPlugins; [
         # nvim-tree
