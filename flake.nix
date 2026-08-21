270cb7b7a0c7ae1fc04cef32610ba63d818b74d5
Michael Barney, Jr <mbarneyme@gmail.com>
2026-08-21 19:27:13 -0400

    feat(cfn-nvim): upgrade to v0.0.2

--- flake.nix
+++ flake.nix
@@ -18,7 +18,7 @@
       inputs.flake-parts.follows = "flake-parts";
     };
     cfn-nvim = {
-      url = "github:mbarneyjr/cfn.nvim/v0.0.1";
+      url = "github:mbarneyjr/cfn.nvim/v0.0.2";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.flake-parts.follows = "flake-parts";
     };
