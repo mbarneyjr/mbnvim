08f2700782c3386c785273bd9e9efdf923f8219d
Michael Barney, Jr <mbarneyme@gmail.com>
2026-08-24 10:21:07 -0400

    feat(cfn.nvim): bump to v0.0.4

--- flake.nix
+++ flake.nix
@@ -18,7 +18,7 @@
       inputs.flake-parts.follows = "flake-parts";
     };
     cfn-nvim = {
-      url = "github:mbarneyjr/cfn.nvim/v0.0.3";
+      url = "github:mbarneyjr/cfn.nvim/v0.0.4";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.flake-parts.follows = "flake-parts";
     };
